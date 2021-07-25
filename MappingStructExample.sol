ca07a8b19ccebe76ff656b0555dc897c044b520c
Azmir Shah <azmirshah1@gmail.com>
2021-07-25 16:25:17 +0800

    solidity data structure - Mapping & Struct

--- MappingStructExample.sol
+++ MappingStructExample.sol
@@ -2,28 +2,44 @@ pragma solidity ^0.5.13;
 
 contract MappingStructExample {
     
-    mapping(address => uint) public balanceReceived;
+    struct Payment {
+        uint amount;
+        uint timestamps;
+        
+    }
+    
+    struct Balance {
+        uint totalBalance;
+        uint numPayments;
+        mapping (uint => Payment) payments;
+    }
+    
+    mapping(address => Balance) public balanceReceived;
     
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
     
     function sendMoney() public payable {
+        balanceReceived[msg.sender].totalBalance += msg.value;
         
-        balanceReceived[msg.sender] += msg.value;
+        Payment memory payment = Payment (msg.value, now);
+        
+        balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments] = payment;
+        balanceReceived[msg.sender].numPayments++;
         
     }
     
     function withdrawMoney(address payable _to, uint _amount) public {
-        require(balanceReceived[msg.sender] >=_amount, "not enought funds");
-        balanceReceived[msg.sender] -= _amount;
+        require(balanceReceived[msg.sender].totalBalance >=_amount, "not enought funds");
+        balanceReceived[msg.sender].totalBalance -= _amount;
         _to.transfer(_amount);
     }
     
     function withdrawAllMoney(address payable _to) public {
         
-        uint balanceToSend =balanceReceived[msg.sender];
-        balanceReceived[msg.sender] =0;
+        uint balanceToSend =balanceReceived[msg.sender].totalBalance;
+        balanceReceived[msg.sender].totalBalance =0;
         _to.transfer(balanceToSend);
     }
 }
