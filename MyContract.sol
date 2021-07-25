e387d75112df2057b1185856ef83a4a3289b672b
Azmir Shah <azmirshah1@gmail.com>
2021-07-25 14:26:43 +0800

    Solidity set address and check address balance

--- MyContract.sol
+++ MyContract.sol
@@ -3,24 +3,39 @@ pragma solidity ^0.5.13;
 contract WorkingWithVariables{
     uint256 public myUint;
     
+    // integers
     function setMyUint(uint _myUint) public {
         myUint = _myUint;
     }
     
     bool public myBool;
     
+    // Set Boolean
     function setMyBool(bool _myBool) public {
         myBool = _myBool;
     }
 
     uint8 public myUint8;
 
+    // Increment Integers
     function incrementUint() public {
         myUint8++;
     }
 
+    // Decrement Integers
     function decrementUint() public {
         myUint8--;
     }
 
+    address public myAddress;
+
+    // Set Address
+    function setAddress(address _address) public  {
+        myAddress = _address;
+    }
+
+    // Check Address Balance
+    function getBalanceOfAddress() public view returns(uint) {
+        return myAddress.balance;
+    }
 }
