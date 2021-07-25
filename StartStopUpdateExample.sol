491b92501e1dfe587cde3ddea5fbfc3035415593
Azmir Shah <azmirshah1@gmail.com>
2021-07-25 16:07:21 +0800

    solidity data structure - Mapping & Struct

--- StartStopUpdateExample.sol
+++ StartStopUpdateExample.sol
@@ -26,4 +26,9 @@ contract StartStopUpdateExample {
        _to.transfer(address(this).balance);
         
     }
+
+    function destroySmartContract(address payable _to) public {
+        require(msg.sender ==owner, "You are not the owner");
+        selfdestruct(_to);
+    }
 }
